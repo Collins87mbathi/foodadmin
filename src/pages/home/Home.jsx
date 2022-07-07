9ff507cebb3db73ae8fdaecd788f84de420cdf49
collins mbathi <perezcoolins@gmail.com>
2022-07-07 05:51:42 +0300

    real user

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,27 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
-
-
+import {BASE_URL} from '../../utils/Constant'
+import axios from 'axios'
+import { useEffect,useState } from "react";
 export default function Home() {
+  const [user, setUser] = useState([]);
+
+  useEffect(()=> {
+    const getUsers = async () => {
+      const res = await axios.get(BASE_URL + '/user/all_information')
+      setUser(res.data);
+    };
+    getUsers();
+  },[]);
+
+
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={user} title="User Analytics" grid dataKey="Active User"/>
       <div className="homeWidgets">
         <WidgetSm/>
       </div>
